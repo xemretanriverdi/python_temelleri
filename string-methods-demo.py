7321949905e86bec4eedca3296b67e793c8445e7
xemretanriverdi <xemretanriverdi@gmail.com>
2020-09-01 03:37:46 +0300

    Create string-methods-demo.py

---
+++ string-methods-demo.py
@@ -0,0 +1,12 @@
+website = "http://www.emretanriverdi.com"
+course = "Python Kursu: Baştan Sona Python PRogramlama Reberiniz (40 saat)"
+
+#1 " Hello World " baş ve son karakter silin
+message= " Hello World "
+message = message.strip()
+print(message)
+
+#2 websitede sadece emretanriverdi kalsın
+
+
+
